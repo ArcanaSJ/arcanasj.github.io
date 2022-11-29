b0eef75cbb162c33f2afffd00fb0c4ee84e89b9d
ArcanaSJ <seanjosepharcana@gmail.com>
2022-11-29 22:36:51 +0800

    Update jsLogin.js

--- JavaScript/jsLogin.js
+++ JavaScript/jsLogin.js
@@ -2,7 +2,7 @@ function auth(){
     var user = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if(user == "admin" && password == "admin"){
-        window.location.assign("myHome.html");
+        window.location.assign('myHome.html');
         alert("Login Successfully!");
     } else {
         alert("Invalid Password or Username!");
